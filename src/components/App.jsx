d7a83155b0eb8a840ba85e57e19605572e6e1e50
trieungochai <trieungochai.dev@gmail.com>
2021-10-07 13:44:04 +0700

    Creating a Footer.jsx component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import Footer from "./Footer";
 import Header from "./Header";
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Hello Cowboy!!!</h1>;
+      <h1>Hello Cowboy!!!</h1>
+      <Footer />
     </div>
   );
 }
