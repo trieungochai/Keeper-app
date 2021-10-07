56400857e78de93111c0f1e36003816402b39258
trieungochai <trieungochai.dev@gmail.com>
2021-10-07 13:34:14 +0700

    Creating a Header.jsx component

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Hello Cowboy!!!</h1>;
+    </div>
+  );
+}
+
+export default App;
